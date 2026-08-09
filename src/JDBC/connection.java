e762d33c9a239989c079d9c945d16a0c817cf8cf
PatelKrish05 <patelkrish54678@gmail.com>
2026-08-09 15:18:06 +0530

    2 commit

---
+++ src/JDBC/connection.java
@@ -0,0 +1,25 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+    static Connection con;
+    protected connection(){
+        String url = "jdbc:mysql://localhost:3306/yatraverse";
+        String username = "root";
+        String password = "";
+
+
+
+        try {
+            con = DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException e) {
+            System.out.println("Server Down...");
+            System.exit(0);
+        }
+    }
+}
+
